5cf78e0fa5d60dea7feb9e36a5d7fbdb7d9f0124
leloufadel <leloufadel@gmail.com>
2023-04-24 19:01:47 +0300

    fix linters

---
+++ main.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.second-nav');
+const span = document.createElement('span');
+span.innerHTML += '<span>&times;</span>';
+span.classList.add('close');
