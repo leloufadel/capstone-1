1856362102d513cd49626605f6bb328e558b7a30
leloufadel <leloufadel@gmail.com>
2023-04-26 22:37:57 +0300

    Add some functionality to about page

--- main.js
+++ main.js
@@ -19,105 +19,96 @@ menu.addEventListener('click', () => {
   }
 });
 
-
-
-window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
-    generateSpeakers();
-});
-
-var speakers = [
-    {
-        image: 'Assets/Aisha-Pandor.webp',
-        name: 'Prof. Toto Titi',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/Amanda-Herson.webp',
-        name: 'Prof. Nana Aba Appiah Amfo',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/Apoorva-Kumar.webp',
-        name: 'Prof. Toto Titi',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/asha-kamenge.webp',
-        name: 'Prof. Lala Fadel',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/candy.webp',
-        name: 'Prof. Tata Toto',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/hartell.webp',
-        name: 'Prof. Tam Tam',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/john-muteru.webp',
-        name: 'Prof. Titi Tato',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/Kevin-Tucker.jpg',
-        name: 'Prof. Foo Var',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/lance.webp',
-        name: 'Prof. Foo Bar',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/Paul-Misener.webp',
-        name: 'Prof. John Doe',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/Sam-Wilson-Spath.webp',
-        name: 'Prof. Sam-Wilson-Spath',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
-    {
-        image: 'Assets/Samantha-Starmer.webp',
-        name: 'Prof. Samantha-Starmer',
-        title: 'Vice-chancellor, University of Ghana.',
-        bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
-    },
+const speakers = [
+  {
+    image: 'Assets/Aisha-Pandor.webp',
+    name: 'Prof. Toto Titi',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/Amanda-Herson.webp',
+    name: 'Prof. Nana Aba Appiah Amfo',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/Apoorva-Kumar.webp',
+    name: 'Prof. Toto Titi',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/asha-kamenge.webp',
+    name: 'Prof. Lala Fadel',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/candy.webp',
+    name: 'Prof. Tata Toto',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/hartell.webp',
+    name: 'Prof. Tam Tam',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/john-muteru.webp',
+    name: 'Prof. Titi Tato',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/Kevin-Tucker.jpg',
+    name: 'Prof. Foo Var',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/lance.webp',
+    name: 'Prof. Foo Bar',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/Paul-Misener.webp',
+    name: 'Prof. John Doe',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/Sam-Wilson-Spath.webp',
+    name: 'Prof. Sam-Wilson-Spath',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
+  {
+    image: 'Assets/Samantha-Starmer.webp',
+    name: 'Prof. Samantha-Starmer',
+    title: 'Vice-chancellor, University of Ghana.',
+    bio: 'Prof. Nana Aba Appiah Amfo is a university administrator and the current Vice-chancellor at UG.',
+  },
 ];
-
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-var chosenSpeaker = [];
+const chosenSpeaker = [];
 function generateSpeakers() {
-    var speakersConainer =  document.querySelector(".speakers-grid");
-    console.log("speakersConainer", speakersConainer);
-    for (var i = 1; i <= 6; i++) {
-        var rndInt = randomIntFromInterval(0, 11);
-        while (chosenSpeaker.includes(rndInt)) {
-            rndInt =  randomIntFromInterval(0, 11);
-        }
-        chosenSpeaker.push(rndInt);
-        var speaker = speakers[rndInt];
-        var speakerElement = document.createElement("div");
-        speakerElement.classList.add("speaker");
-        speakerElement.innerHTML = `
+  const speakersConainer = document.querySelector('.speakers-grid');
+  for (let i = 1; i <= 6; i += 1) {
+    let rndInt = randomIntFromInterval(0, 11);
+    while (chosenSpeaker.includes(rndInt)) {
+      rndInt = randomIntFromInterval(0, 11);
+    }
+    chosenSpeaker.push(rndInt);
+    const speaker = speakers[rndInt];
+    const speakerElement = document.createElement('div');
+    speakerElement.classList.add('speaker');
+    speakerElement.innerHTML = `
             <div class="speaker-img">
                 <img src="${speaker.image}" alt="">
             </div>
@@ -131,8 +122,11 @@ function generateSpeakers() {
                 ${speaker.bio}
                 </p>
             </div>
-        `
+        `;
 
-        speakersConainer.appendChild(speakerElement);
-    }
+    speakersConainer.appendChild(speakerElement);
+  }
 }
+window.addEventListener('DOMContentLoaded', () => {
+  generateSpeakers();
+});
