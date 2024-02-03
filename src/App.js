036386b15def3c92978d457beaa3d791f5ca2f19
leloufadel <leloufadel@gmail.com>
2024-02-03 08:38:06 +0000

    Add component Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import NavBar from './NavBar';
 
-function App() {
+const AppContainer = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <NavBar />
+    </AppContainer>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+    <nav>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Guests</a></li>
+        <li><a href="/">Events</a></li>
+
+    </ul>
+    </nav>
+
+    )
+
+}
+export default Navbar;
