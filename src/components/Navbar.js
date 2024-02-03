5f7338b971a41513f0f990c61f2da21b6df5a113
leloufadel <leloufadel@gmail.com>
2024-02-03 10:13:39 +0000

    fix js linters

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,23 @@
-import React from "react";
+import React from 'react';
 import './stylesheets/navbar.css';
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-        <div className="logo">My Website</div>
-        <div className="nav-items">
-        <ul className="overview">
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="logo">My Website</div>
+    <div className="nav-items">
+      <ul className="overview">
         <li><a href="/">Home</a></li>
         <li><a href="/">About</a></li>
         <li><a href="/">Guests</a></li>
         <li><a href="/">Events</a></li>
 
-    </ul>
-    <ul className="account">
+      </ul>
+      <ul className="account">
         <li><a href="/">Register</a></li>
         <li><a href="/">Language</a></li>
-    </ul>
+      </ul>
     </div>
-    </nav>
+  </nav>
 
-    )
-
-}
+);
 export default Navbar;
